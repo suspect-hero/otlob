520a5650ec7caa108a5729bc1ca6f877e81dc247
ahmed <ahmed@ahmed-desktop.(none)>
2013-01-16 19:27:54 +0200

    first checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talabatak::Application.config.secret_token = '4a761c4d817ebd472d6209db81669df7dac15503a99466c8e329499c537f70b256aaf707f59e368d8f1127978d9010bce541aafbe4c10aa5bdbac5dbe9b16ba7'
